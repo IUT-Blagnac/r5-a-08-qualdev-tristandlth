e46640c771d0f2e74ecc61383563c091c63b7452
tristandlth <85364501+tristandlth@users.noreply.github.com>
2025-01-06 14:25:10 +0100

    TP1

---
+++ hellocucumber/src/test/java/hellocucumber/StepDefinitions.java
@@ -0,0 +1,51 @@
+package hellocucumber;
+
+import io.cucumber.java.en.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Assertions.*;
+
+public class StepDefinitions {
+
+    @Given("an example scenario")
+    public void anExampleScenario() {
+    }
+
+    @When("all step definitions are implemented")
+    public void allStepDefinitionsAreImplemented() {
+    }
+
+    @Then("the scenario passes")
+    public void theScenarioPasses() {
+    }
+
+    private String today;
+    private String actualAnswer;
+    
+    @Given("today is Sunday")
+    public void today_is_sunday() {
+        today = "Sunday";
+    }
+
+    @Given("today is Friday")
+    public void today_is_friday() {
+        today = "Friday";
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_friday_yet() {
+        actualAnswer = IsItFriday.isItFriday(today);
+    }
+
+    @Then("I should be told {string}")
+    public void i_should_be_told(String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer);
+    }
+}
+
+class IsItFriday {
+    static String isItFriday(String today) {
+        return "Friday".equals(today) ? "TGIF" : "Nope";
+    }
+}
